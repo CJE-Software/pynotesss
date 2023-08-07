767ced3b854a81286bca93c52637741e291587e0
CJ Evans <cje.software.engineer@gmail.com>
2023-08-07 08:23:36 -0500

    added more specific search params

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ count = 0
 for line in fhand:
     count = count + 1
 print('Line Count: ', count)
+
+
+#Reading the WHOLE file by character
+#fhand = open('biblekjv.txt')
+#inp = fhand.read()
+#print(len(inp))
+#print(inp[0:20])....prints only first 20 characters...first 20 char
+
+fhand = open('biblekjv.txt')
+for line in fhand:
+    if line.startswith('Jud') :
+        print(line)
+
+#the above script looks for all lines in text file that start with chars J-U-D
+
+#above code revised to remove newline whitespace
+fhand = open('biblekjv.txt')
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith('Am') :
+        print(line)
